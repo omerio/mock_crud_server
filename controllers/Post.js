0ccc52be7f5e5d14c370e76971edb3e77faac48a
Eamon Doyle <eamon.doyle@mastercard.com>
2016-09-09 19:01:45 +0100

    Check consumer key for create Post

--- controllers/Post.js
+++ controllers/Post.js
@@ -7,6 +7,20 @@ var Post = require('./PostService');
 
 
 module.exports.addPost = function addPost (req, res, next) {
+    var authorizationValues = req.headers.authorization.split(",")
+
+    for (var index in authorizationValues) {
+        var map = authorizationValues[index].split("=")
+
+        if(map[0].trim() == "oauth_consumer_key") {
+            if (map[1].trim() != "\"xaZfTu56TPBzFTxypjmRFySRLxzJcQ1m2vqtwDUY34bf5d25!ede9b31b31d14a68b91bad48dc0a1e400000000000000000\"") {
+                res.writeHead(500, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({error: "Consumer Key should be: xaZfTu56TPBzFTxypjmRFySRLxzJcQ1m2vqtwDUY34bf5d25!ede9b31b31d14a68b91bad48dc0a1e400000000000000000"}));
+                return;
+            }
+        }
+    }
+
   Post.addPost(req.swagger.params, res, next);
 };
 
