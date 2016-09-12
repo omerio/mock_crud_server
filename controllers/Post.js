400b9eef9c86a6d299c9d22a28ae95afa80a4b57
Eamon Doyle <eamon.doyle@mastercard.com>
2016-09-12 12:10:01 +0100

    consumer key check for send_1 for create Post API

--- controllers/Post.js
+++ controllers/Post.js
@@ -1,24 +1,28 @@
 'use strict';
 
 var url = require('url');
+var oauth = require('../utils/oauth')
 
 
 var Post = require('./PostService');
 
 
 module.exports.addPost = function addPost (req, res, next) {
-    var authorizationValues = req.headers.authorization.split(",")
+    var send_1 = "xaZfTu56TPBzFTxypjmRFySRLxzJcQ1m2vqtwDUY34bf5d25!ede9b31b31d14a68b91bad48dc0a1e400000000000000000";
+    var type = {'Content-Type': 'application/json'};
 
-    for (var index in authorizationValues) {
-        var map = authorizationValues[index].split("=")
+    var consumerKey = oauth.getConsumerKey(req);
 
-        if(map[0].trim() == "oauth_consumer_key") {
-            if (map[1].trim() != "\"xaZfTu56TPBzFTxypjmRFySRLxzJcQ1m2vqtwDUY34bf5d25!ede9b31b31d14a68b91bad48dc0a1e400000000000000000\"") {
-                res.writeHead(500, {'Content-Type': 'application/json'});
-                res.end(JSON.stringify({error: "Consumer Key should be: xaZfTu56TPBzFTxypjmRFySRLxzJcQ1m2vqtwDUY34bf5d25!ede9b31b31d14a68b91bad48dc0a1e400000000000000000"}));
-                return;
-            }
-        }
+    if (!consumerKey) {
+        res.writeHead(500, type);
+        res.end(JSON.stringify({error: "No Consumer Key found in Authorisation header. Consumer Key should be: " + send_1}));
+        return;
+    }
+
+    if (consumerKey != "\"xaZfTu56TPBzFTxypjmRFySRLxzJcQ1m2vqtwDUY34bf5d25!ede9b31b31d14a68b91bad48dc0a1e400000000000000000\"") {
+        res.writeHead(500, type);
+        res.end(JSON.stringify({error: "Consumer Key should be: " + send_1}));
+        return;
     }
 
   Post.addPost(req.swagger.params, res, next);
